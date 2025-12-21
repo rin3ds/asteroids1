12f469ef482152fe655745bffdc9324ff4ab05e7
rin3ds <rin.estuita@gmail.com>
2025-12-22 04:00:49 +0800

    player asteroid movement created

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Asteroids")
+
+
+clock = pygame.time.Clock()
+dt = 0
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable,)
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+asteroid_field = AsteroidField()
 
 run = True
 while run:
@@ -12,9 +31,16 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        
+    
+    updatable.update(dt)
     screen.fill("black")
+    
+    for d in drawable:
+        d.draw(screen)
+
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
+
 
 
 def main():
