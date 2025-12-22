fa0d1149c50b1a7cb4e72a51a9afa2cd48e4757b
rin3ds <rin.estuita@gmail.com>
2025-12-23 02:43:43 +0800

    updated asteroid split

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -14,6 +16,26 @@ class Asteroid(CircleShape):
             self.radius,
             LINE_WIDTH
         )
+    
+    def split(self):
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            Asteroid.kill(self)
+        else:
+            log_event("asteroid_split")
+            
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            angle1 = random.uniform(20, 50)
+            angle2 = -random.uniform(20, 50)
+            velocity1 = self.velocity.rotate(angle1)
+            velocity2 = self.velocity.rotate(angle2)
+
+            Asteroid(self.position.x, self.position.y, new_radius).velocity = velocity1
+            Asteroid(self.position.x, self.position.y, new_radius).velocity = velocity2
+
+            
+            self.kill()
+            
+
 
     def update(self, dt):
         self.position += self.velocity * dt
